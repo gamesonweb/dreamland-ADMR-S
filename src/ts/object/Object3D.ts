2bc893c8865b2975bbcfc79160bee54ba4bf265b
Adam MIR-SADJADI <mirsadjadi2011@yahoo.fr>
2025-05-30 09:13:18 +0200

    Revert "copied current private repo to gamesonweb repo"
    This reverts commit 44fcbe8e9c4962811212e383faecc7abf88fb35f.

--- src/ts/object/Object3D.ts
+++
@@ -1,8 +0,0 @@
-import {Mesh} from "@babylonjs/core/Meshes/mesh";
-import {Scene} from "@babylonjs/core/scene";
-
-export interface Object3D {
-  mesh: Mesh;
-  loadMesh?: () => Mesh;
-  enableInteractions?: (scene: Scene) => void;
-}
