5c1be2749f43cc01978fbabff872cdfd31a85308
Adam MIR-SADJADI <mirsadjadi2011@yahoo.fr>
2025-05-30 09:12:53 +0200

    Revert "push du jeu de puzzle"
    This reverts commit bf2ec50f75e67e7240953035f11d833a1348bfe1.

--- puzzle_3d/src/index.ts
+++
@@ -1,8 +0,0 @@
-// src/index.ts
-import { Game } from './game';
-import './style.css'; // Importe le CSS pour que Webpack le gère
-
-document.addEventListener('DOMContentLoaded', () => {
-    const game = new Game('gameCanvas', 3, 2); // Crée un puzzle 3x3
-    game.run();
-});
